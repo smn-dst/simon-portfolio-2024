4fd37d4de6727d89919a608e4ff8ca79a20cc36b
Freeeemon <simon.dousset23@gmail.com>
2024-08-06 16:51:46 +0200

    redirection 1.6

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,21 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    outDir: 'dist', // Ensure this matches your Vercel settings
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        agori: path.resolve(__dirname, 'agori.html'),
+        artdam: path.resolve(__dirname, 'artdam.html'),
+        bryte: path.resolve(__dirname, 'bryte.html'),
+        opteven: path.resolve(__dirname, 'opteven.html'),
+        projects: path.resolve(__dirname, 'projects.html'),
+        proxinnov: path.resolve(__dirname, 'proxinnov.html'),
+        solware: path.resolve(__dirname, 'solware.html')
+      }
+    }
   }
 });

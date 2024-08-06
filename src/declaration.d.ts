1f70504b0414b702cb6c24bf9d9c530b63a7408c
Freeeemon <simon.dousset23@gmail.com>
2024-08-06 14:56:55 +0200

    declare 5.0

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,10 +1,14 @@
 declare module '*.webp';
 declare module '*.mov';
+
 declare module '*.ts';
 declare module '*.glsl';
+
 declare module '*.scss';
 declare module '*.css';
+
 declare module '*.woff2';
 declare module '*.woff';
+
 declare module 'spltjs';
 declare module 'animejs';
